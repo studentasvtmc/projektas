5e2294417d779af589a77838f453d93023bf07da
Tadas Mekuska <tadodezute@gmail.com>
2022-06-27 17:34:17 +0300

    init commit

---
+++ backend/src/main/java/lt/vtmc/projektas/ProjektasApplication.java
@@ -0,0 +1,13 @@
+package lt.vtmc.projektas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/lt/vtmc/projektas/ProjektasApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.vtmc.projektas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
